bac9d9174c20ee2b076bd2b5e6a2e827ba494896
rodrigoTerenci <rodrigoterenci@gmail.com>
2022-01-27 14:18:50 +0000

    add a index.html

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 import express from 'express'
 import http from 'http'
-i
+
 
 const
     app = express(),
     server = http.createServer(app),
     port = process.env.PORT || 3000
-    
-     robo = robo().then((response)=> console.log(response.data))
     app.get('/',(req,res)=>
-    res.send(robo)
-    //res.render('index')
-   )
+        //res.send(index)
+        res.render('./index.html')
+    )
    
   server.listen(port)
