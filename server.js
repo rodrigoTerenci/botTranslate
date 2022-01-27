7c328572205e23c8185f2e9440b2700a4c652f72
rodrigoTerenci <rodrigoterenci@gmail.com>
2022-01-27 14:50:20 +0000

    https://stackoverflow.com/questions/58134793/error-while-loading-shared-libraries-libnss3-so-while-running-gtlab-ci-job-to

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import http from 'http'
-
+import { renderFile } from 'ejs'
 
 const
     app = express(),
     server = http.createServer(app),
     port = process.env.PORT || 3000
+    app.engine('html', renderFile)
     app.get('/',(req,res)=>
         //res.send(index)
         res.render('./index.html')
