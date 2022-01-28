6064a2ae36c0ba9a1db7a235afffb81272553c4c
rodrigoTerenci <rodrigoterenci@gmail.com>
2022-01-28 17:23:26 +0000

    not yet

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ async function robo() {
   const page = await browser.newPage();
   const linguaBase = readlineSync.question('Informe uma lingua base: ') || 'português';
   const linguaFinal = readlineSync.question('Informe uma lingua desejada:') || 'inglês';
-
+  const valor = readlineSync.question('Informe uma frase: ') || 'test'
   const qualquerUrl = `https://www.google.com/search?q=tradutor+${linguaBase}+para+${linguaFinal}&sxsrf=ALeKk014ocPYlaGkEBv7yQ7esnJB-smI7w%3A1627486563237&ei=Y3kBYfbKDYrR1sQP24mY4AI&oq=tradutor&gs_lcp=Cgdnd3Mtd2l6EAMYATIHCCMQsAMQJzIHCAAQRxCwAzIHCAAQRxCwAzIHCAAQRxCwAzIHCAAQRxCwAzIHCAAQRxCwAzIHCAAQRxCwAzIHCAAQRxCwAzIHCAAQRxCwAzIHCAAQsAMQQ0oECEEYAFAAWABgxRloAXACeACAAYQBiAGEAZIBAzAuMZgBAKoBB2d3cy13aXrIAQrAAQE&sclient=gws-wiz`;
-  await page.goto(qualquerUrl);
+  await page.goto(qualquerUrl)
   // await page.screenshot({path: 'example.png'});
 
 
 
- const resultado = await page.evaluate(() => {
-    document.getElementById('tw-source').innerText = 'teste'
-    return document.querySelector('#tw-target').textContent;
+ const resultado = await page.evaluate((valor) => {
+    document.getElementById('tw-source').textContent = valor
+    return document.getElementById('tw-target').textContent
+    //document.querySelector('#tw-target-text').textContent
   });
   console.log(resultado)
    await browser.close();
